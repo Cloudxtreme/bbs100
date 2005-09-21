4343acd9e0a117a450300c3c41bcdd869816e656
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2005-09-21 16:57:47 +0000

    added NewUserLog
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/bbs100@597 d90a1141-1c03-0410-82d0-813645769019

---
+++ src/NewUserLog.c
@@ -0,0 +1,162 @@
+/*
+    bbs100 3.0 WJ105
+    Copyright (C) 2005  Walter de Jong <walter@heiho.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+	NewUserLog.c	WJ105
+*/
+
+#include "config.h"
+#include "NewUserLog.h"
+#include "CachedFile.h"
+#include "FileFormat.h"
+#include "Memory.h"
+#include "Param.h"
+#include "Timer.h"
+#include "cstring.h"
+#include "log.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+NewUserLog *new_NewUserLog(void) {
+	return (NewUserLog *)Malloc(sizeof(NewUserLog), TYPE_NEWUSERLOG);
+}
+
+void destroy_NewUserLog(NewUserLog *l) {
+	Free(l);
+}
+
+NewUserQueue *load_NewUserQueue(char *filename) {
+NewUserQueue *q;
+File *f;
+int version;
+int (*load_func)(File *, NewUserQueue *);
+
+	if ((q = new_NewUserQueue()) == NULL)
+		return NULL;
+
+	if ((f = Fopen(filename)) == NULL) {
+		log_err("load_NewUserQueue(): failed to open file '%s'", filename);
+		destroy_NewUserQueue(q);
+		return NULL;
+	}
+	version = fileformat_version(f);
+	switch(version) {
+		case -1:
+			log_err("load_NewUserQueue(): error trying to determine file format version of %s", filename);
+			load_func = NULL;
+			break;
+
+		case 1:
+			load_func = load_NewUserQueue_version1;
+			break;
+
+		default:
+			log_err("load_User(): don't know how to load file format version %d of %s", version, filename);
+	}
+	if (load_func != NULL && !load_func(f, q)) {
+		Fclose(f);
+		return q;
+	}
+	Fclose(f);
+	destroy_NewUserQueue(q);
+	return NULL;
+}
+
+int load_NewUserQueue_version1(File *f, NewUserQueue *q) {
+NewUserLog *l;
+char buf[PRINT_BUF], *p;
+int ff1_continue;
+
+	if (f == NULL || q == NULL)
+		return -1;
+
+	while(Fgets(f, buf, PRINT_BUF) != NULL) {
+		FF1_PARSE;
+
+		if (!strcmp(buf, "newuser")) {
+			if ((l = new_NewUserLog()) == NULL)
+				FF1_ERROR;
+
+			l->timestamp = cstrtoul(p, 10);
+
+			if ((p = cstrchr(p, ' ')) == NULL) {
+				destroy_NewUserLog(l);
+				FF1_ERROR;
+			}
+			p++;
+			if (!*p) {
+				destroy_NewUserLog(l);
+				FF1_ERROR;
+			}
+			cstrcpy(l->name, p, MAX_NAME);
+
+			add_newuserlog(q, l);
+		}
+	}
+	return 0;
+}
+
+int save_NewUserQueue(NewUserQueue *q, char *filename) {
+File *f;
+NewUserLog *l;
+
+	if ((f = Fcreate(filename)) == NULL)
+		return -1;
+
+	FF1_SAVE_VERSION;
+
+	for(l = (NewUserLog *)q->tail; l != NULL; l = l->next)
+		Fprintf(f, "newuser=%lu %s", l->timestamp, l->name);
+
+	Fclose(f);
+	return 0;
+}
+
+void add_newuserlog(NewUserQueue *newusers, NewUserLog *l) {
+NewUserLog *old;
+
+	while(count_Queue(newusers) >= PARAM_MAX_NEWUSERLOG
+		&& (old = pop_NewUserQueue(newusers)) != NULL)
+		destroy_NewUserLog(old);
+
+	add_NewUserQueue(newusers, l);
+}
+
+void log_newuser(char *name) {
+NewUserQueue *newusers;
+NewUserLog *l;
+
+	if (name == NULL || !*name || (l = new_NewUserLog()) == NULL)
+		return;
+
+	l->timestamp = (unsigned long)rtc;
+	cstrcpy(l->name, name, MAX_NAME);
+
+	if ((newusers = load_NewUserQueue(PARAM_NEWUSERLOG)) == NULL
+		&& (newusers = new_NewUserQueue()) == NULL) {
+		log_err("log_newuser(%s) failed, out of memory?", name);
+		return;
+	}
+	add_newuserlog(newusers, l);
+	save_NewUserQueue(newusers, PARAM_NEWUSERLOG);
+	destroy_NewUserQueue(newusers);
+}
+
+/* EOB */
---
+++ src/include/NewUserLog.h
@@ -0,0 +1,75 @@
+/*
+    bbs100 3.0 WJ105
+    Copyright (C) 2005  Walter de Jong <walter@heiho.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+	NewUserLog.h	WJ105
+*/
+
+#ifndef NEWUSERLOG_H_WJ105
+#define NEWUSERLOG_H_WJ105	1
+
+#include "List.h"
+#include "Queue.h"
+#include "CachedFile.h"
+#include "defines.h"
+
+#define add_NewUserLog(x,y)			(NewUserLog *)add_List((x), (y))
+#define prepend_NewUserLog(x,y)		(NewUserLog *)prepend_List((x), (y))
+#define remove_NewUserLog(x,y)		(NewUserLog *)remove_List((x), (y))
+#define concat_NewUserLog(x,y)		(NewUserLog *)concat_List((x), (y))
+#define pop_NewUserLog(x)			(NewUserLog *)pop_List(x)
+#define listdestroy_NewUserLog(x)	listdestroy_List((x), destroy_NewUserLog)
+#define rewind_NewUserLog(x)		(NewUserLog *)rewind_List(x)
+#define unwind_NewUserLog(x)		(NewUserLog *)unwind_List(x)
+#define sort_NewUserLog(x, y)		(NewUserLog *)sort_List((x), (y))
+
+#define add_NewUserQueue(x,y)		(NewUserLog *)add_Queue((x), (y))
+#define remove_NewUserQueue(x,y)	(NewUserLog *)remove_Queue((x), (y))
+#define concat_NewUserQueue(x,y)	(NewUserLog *)concat_Queue((x), (y))
+#define pop_NewUserQueue(x)			(NewUserLog *)pop_Queue(x)
+#define dequeue_NewUserQueue(x)		(NewUserLog *)dequeue_Queue(x)
+#define new_NewUserQueue			new_Queue
+#define destroy_NewUserQueue(x)		destroy_Queue((x), destroy_NewUserLog)
+#define deinit_NewUserQueue(x)		deinit_Queue((x), destroy_NewUserLog)
+#define sort_NewUserQueue(x, y)		sort_Queue((x), (y))
+
+typedef QueueType NewUserQueue;
+
+typedef struct NewUserLog_tag NewUserLog;
+
+struct NewUserLog_tag {
+	List(NewUserLog);
+
+	unsigned long timestamp;
+	char name[MAX_NAME];
+};
+
+NewUserLog *new_NewUserLog(void);
+void destroy_NewUserLog(NewUserLog *);
+
+NewUserQueue *load_NewUserQueue(char *);
+int load_NewUserQueue_version1(File *, NewUserQueue *);
+int save_NewUserQueue(NewUserQueue *, char *);
+
+void add_newuserlog(NewUserQueue *, NewUserLog *);
+
+void log_newuser(char *);
+
+#endif	/* NEWUSERLOG_H_WJ105 */
+
+/* EOB */
